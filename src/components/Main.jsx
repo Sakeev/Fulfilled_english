e347e10efdd18cb48eff9460e37ad71a7fb3c344
Sultan <bekovs96@gmail.com>
2022-09-29 11:35:13 +0600

    main page progress bar added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
             <Box sx={{display: 'flex', width: '100%', justifyContent: 'space-between'}}>
                 <Box sx={{width: '65%'}}>
                     <Paper elevation={3} sx={{m: 2,height: '28vh', maxHeight: "220px", width: "100%", p: 2, bgcolor:'#EDF6F9', 
-                        borderRadius: "50px 10px 10px", display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
+                        borderRadius: "50px 10px 10px", display: 'flex', justifyContent: 'space-around', alignItems: 'center'}} onClick={()=>alert('вы начали занятие')}>
                         <Box sx={{height: '100px', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
                             <Typography variant="p" sx={{color: '#83C5BE'}}>До занятия осталось: 5:43</Typography>
                             <Typography variant="h5" sx={{color: '#006D77'}}>Начать занятие</Typography>
@@ -32,8 +32,11 @@ const Main = () => {
 
             <Box >
                 <Paper elevation={3} sx={{ m: 2,height: '28vh', maxHeight: "220px", width: "100%", p: 2, bgcolor:'#EDF6F9', 
-                    borderRadius: "10px"}}>
-                    Прогресс ученика
+                    borderRadius: "10px", display: 'flex', flexDirection:'column', justifyContent: 'space-evenly', alignItems: 'space-between'}}>
+                    <Typography variant="h6" sx={{ml: 5, color: '#006d77'}}>Ваш прогресс</Typography>
+                    <Box sx={{ml: 5, width: '90%', height:'50px', bgcolor: '#83C5BE', borderRadius: '10px'}}>
+                        <Box sx={{width: '70%', height: '100%', bgcolor: '#E29578', borderRadius: '10px'}}></Box>
+                    </Box>
                 </Paper>
             </Box>
 
@@ -41,13 +44,13 @@ const Main = () => {
                 <Box sx={{width: '47%'}}>
                     <Paper elevation={3} sx={{ m: 2,height: '28vh', maxHeight: "220px", width: "100%", p: 2, bgcolor:'#EDF6F9',  
                         borderRadius: "10px 10px 10px 50px"}}>
-                        Баланс
+                        Расписание (в разработке)
                     </Paper>
                 </Box>
                 <Box sx={{width: '47%'}}>
                     <Paper elevation={3} sx={{ m: 2,height: '28vh', maxHeight: "220px", width: "100%", p: 2, bgcolor:'#EDF6F9',  
                         borderRadius: "10px 10px 50px 10px"}}>
-                        Словарь
+                        Словарь (в разработке)
                     </Paper>
                 </Box>
             </Box>
