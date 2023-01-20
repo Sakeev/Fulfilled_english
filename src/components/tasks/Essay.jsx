11df925cd4dad860f13027028eea1aaa3f64a677
Jaanger123 <jbarakanov@inbox.ru>
2023-01-20 12:18:14 +0600

    ready to merge with authLogic

--- src/components/tasks/Essay.jsx
+++ src/components/tasks/Essay.jsx
@@ -3,29 +3,91 @@ import axios from 'axios';
 import React from 'react';
 import { useState } from 'react';
 
-const Essay = () => {
-    const [essey , setEssay] = useState('');
-    let API=`sulaLoh228`
+const btnStyle = {
+    margin: '10px 5px',
+    backgroundColor: '#9bd0cb',
+    color: '#006D77',
+    textTransform: 'upper',
+    '&:hover': {
+        backgroundColor: '#006D77',
+        color: '#9bd0cb',
+    },
+};
 
-    const sendEssay = async (essey)=>{
-       const data =  await axios.post(API , essey);
-    }
+const Essay = () => {
+    const [essay, setEssay] = useState('');
+    console.log(essay);
+    const API = 'http://35.238.162.84/room/essa/';
+
+    const sendEssay = async (essay) => {
+        const data = await axios.get(API);
+        console.log(data);
+        // const data = await axios.post(API, essay);
+    };
+
     return (
-        <div style={{width:"100%" , height:"100%"}}>
-            <Box sx={{paddingLeft:"20%" , paddingTop:"5%" , width:"100%" , height:"100%" , display:"flex" , flexDirection:"column"}}>
-                <Typography sx={{paddingBottom:"5%" , color:"#006D77" , fontWeight:"bold" , fontSize:"32px"}}>
+        <div style={{ width: '100%', height: '100%' }}>
+            <Box
+                sx={{
+                    paddingLeft: '20%',
+                    paddingTop: '5%',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                <Typography
+                    sx={{
+                        paddingBottom: '5%',
+                        color: '#006D77',
+                        fontWeight: 'bold',
+                        fontSize: '32px',
+                    }}
+                >
                     Essay
                 </Typography>
-                <Box sx={{display:"flex" , alignItems:"center"  , paddingBottom:"5%"}}>
-                <Typography sx={{paddingRight:"51.5%" , color:"#006D77" , fontSize:"24px" , fontWeight:"bold"}}>
-                    Тема:
-                </Typography>
-                <Button sx={{backgroundColor:"#83C5BE" , color:"#006D77"}}>перевести</Button>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        paddingBottom: '5%',
+                    }}
+                >
+                    <Typography
+                        sx={{
+                            paddingRight: '51.5%',
+                            color: '#006D77',
+                            fontSize: '24px',
+                            fontWeight: 'bold',
+                        }}
+                    >
+                        Тема:
+                    </Typography>
+                    <Button sx={btnStyle}>перевести</Button>
                 </Box>
-                <input onChange={(e)=>setEssay(e.target.value)} style={{width:"70%" , height:"50%" , borderRadius:"22px" , border:"2px solid #006D77" , marginBottom:"5%"}} ></input>
-                <Button onClick={()=>sendEssay(essey)} sx={{backgroundColor:"#83C5BE" , color:"#006D77" , width:"12%" }}>Отправить</Button>
+                <textarea
+                    onChange={(e) => setEssay(e.target.value)}
+                    style={{
+                        width: '70%',
+                        height: '50%',
+                        borderRadius: '16px',
+                        border: '2px solid #006D77',
+                        marginBottom: '5%',
+                        padding: '0.5rem 0.75rem',
+                        resize: 'none',
+                    }}
+                ></textarea>
+                <Button
+                    onClick={() => sendEssay(essay)}
+                    sx={{
+                        ...btnStyle,
+                        width: '12%',
+                    }}
+                >
+                    Отправить
+                </Button>
             </Box>
-            
         </div>
     );
 };
