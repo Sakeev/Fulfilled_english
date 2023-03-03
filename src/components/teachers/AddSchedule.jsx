1cf54e78848a90a769de84de3ddaa4c4b28556c4
sultan bekov <bekovs96@gmail.com>
2023-03-03 19:59:26 +0600

    styles done in schedule page

--- src/components/teachers/AddSchedule.jsx
+++ src/components/teachers/AddSchedule.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useUsers } from '../../contexts/UsersContextProvider';
 import "./Schedule.css"
 
-const AddSchedule = ({time, setShowInps}) => {
+const AddSchedule = ({info = {weekday: '?', time: '?'}, setShowInps}) => {
+  const { getStudents, studentsList } = useUsers();
+  const [student, setStudent] = useState({})
+
+  useEffect(() => {
+    getStudents();
+  }, [])
 
   document.addEventListener('keydown', (e) => {
     if(e.key == 'Escape') {
@@ -9,15 +17,23 @@ const AddSchedule = ({time, setShowInps}) => {
     }
   })
 
+  const handleButton = () => {
+    console.log(student)
+  }
+
   return (
     <div className='overlay' onClick={() => setShowInps(false)}>
       <div className='modal' onClick={(e) => e.stopPropagation()}>
-        <select name="" id="">
-          <option>Carlsen</option>
-          <option>Ronaldo</option>
-          <option>David</option>
+        <h4>{info.weekday} {info.time}</h4>
+        <select name="" id="" onChange={(e) => setStudent(JSON.parse(e.target.value))}>
+          <option value="" selected hidden></option>
+          {
+            studentsList.map((student, index) => (
+              <option key={index} value={JSON.stringify(student)}>{student.username}</option>
+            ))
+          }
         </select>
-        <button>accept</button>
+        <button onClick={handleButton}>accept</button>
       </div>
     </div>
   );
--- src/components/teachers/ScheduleTeachers.jsx
+++ src/components/teachers/ScheduleTeachers.jsx
@@ -1,53 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useSchedule } from '../../contexts/ScheduleContextProvider';
+import { useUsers } from '../../contexts/UsersContextProvider';
 import AddSchedule from './AddSchedule';
 import "./Schedule.css"
+import { getWeekDay } from '../../helpers/funcs';
 
 const rows = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17 ,18, 19, 20, 21, 22, 23, 24];
+const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
-const schedule_test = [
-  {
-    time: '8:00',
-    weekday: 'Monday',
-    student: 'David Ghetto',
-    credits: 9,
-  },
-  {
-    time: '17:00',
-    weekday: 'Monday',
-    student: 'Salomon Peters',
-    credits: 7,
-  },
-  {
-    time: '9:00',
-    weekday: 'Tuesday',
-    student: 'Magnus Skewer',
-    credits: 88,
-  },
-  {
-    time: '11:00',
-    weekday: 'Wendesday',
-    student: 'Ronaldo SUII',
-    credits: 77,
-  },
-  {
-    time: '20:00',
-    weekday: 'Friday',
-    student: 'David Guetta',
-    credits: 121,
-  }
-]
-
-
-const setLesson = (lessons = schedule_test, time, weekday) => {
-  let lesson = lessons.find((les) => les.time === `${time}:00` && les.weekday === weekday)
+const setLesson = (lessons, time, weekday) => {
+  let lesson = lessons.find((les) => les.time === `${time}:00:00` && les.weekday === weekday)
   return (
     <>
-        { 
+        {
           lesson ? 
           <div className='lesson_block'>
-            <p>{lesson.student}</p>
-            <p>credits: <b>{lesson.credits}</b></p>
+            <p>{lesson.info.name}</p>
+            <p><b>{lesson.accepted ? "accepted" : "not accepted"}</b></p>
           </div>
           :
           <></>
@@ -68,45 +37,45 @@ const table_style = {
 
 const ScheduleTeachers = () => {
   const { getSchedule, schedule } = useSchedule();
-  const [showInps, setShowInps] = useState(false)
+  const [showInps, setShowInps] = useState(false);
+  const [dayInfo, setDayInfo] = useState({});
 
   useEffect(() => {
     getSchedule();
   }, [])
 
-  console.log(schedule)
-  
   return (
     <div style={container}>
       <table style={table_style} className="table_schedule">
         <tr>
           <th>GMT+3</th>
-          <th>Mon</th>
-          <th>Tue</th>
-          <th>Wed</th>
-          <th>Thu</th>
-          <th>Fri</th>
-          <th>Sat</th>
-          <th>Sun</th>
+          <th className={getWeekDay() == 'Monday' ? 'today-active' : ''}>Mon</th>
+          <th className={getWeekDay() == 'Tuesday' ? 'today-active' : ''}>Tue</th>
+          <th className={getWeekDay() == 'Wendesday' ? 'today-active' : ''}>Wed</th>
+          <th className={getWeekDay() == 'Thursday' ? 'today-active' : ''}>Thu</th>
+          <th className={getWeekDay() == 'Friday' ? 'today-active' : ''}>Fri</th>
+          <th className={getWeekDay() == 'Saturday' ? 'today-active' : ''}>Sat</th>
+          <th className={getWeekDay() == 'Sunday' ? 'today-active' : ''}>Sun</th>
         </tr>
         {
           rows.map((row, index) => (
             <tr key={index}>
               <td onClick={()=>setShowInps(!showInps)}>{`${row}:00`}</td>
-              <td onClick={()=>setShowInps(!showInps)}>{setLesson(schedule_test, row, 'Monday')}</td>
-              <td onClick={()=>setShowInps(!showInps)}>{setLesson(schedule_test, row, 'Tuesday')}</td>
-              <td onClick={()=>setShowInps(!showInps)}>{setLesson(schedule_test, row, 'Wendesday')}</td>
-              <td onClick={()=>setShowInps(!showInps)}>{setLesson(schedule_test, row, 'Thursday')}</td>
-              <td onClick={()=>setShowInps(!showInps)}>{setLesson(schedule_test, row, 'Friday')}</td>
-              <td onClick={()=>setShowInps(!showInps)}>{setLesson(schedule_test, row, 'Saturday')}</td>
-              <td onClick={()=>setShowInps(!showInps)}>{setLesson(schedule_test, row, 'Sunday')}</td>
+              {
+                weekdays.map(weekday => (
+                  <td onClick={()=>{
+                    setShowInps(!showInps)
+                    setDayInfo({weekday, time: `${row}:00`})
+                  }}>{setLesson(schedule, row, weekday)}</td>
+                ))
+              }
             </tr>
           ))
         }
       </table>
       {
         showInps ?
-        <AddSchedule setShowInps={setShowInps} />
+        <AddSchedule setShowInps={setShowInps} info={dayInfo} />
         :
         <></>
       }
