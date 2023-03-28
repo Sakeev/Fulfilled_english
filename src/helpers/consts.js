ed12c5417456b229f2094cd9d684544cb2de908a
Sultan <32393613+bekovs@users.noreply.github.com>
2023-03-28 10:06:43 +0600

    Update consts.js

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,6 +1,6 @@
 export const ADMIN = "admin@admin.com";
 
 export const API = "http://35.238.162.84/";
-export const AUTH_API = "http://35.238.162.84/account/api/token/";
+// export const AUTH_API = "http://35.238.162.84/account/api/token/";
 export const SCHEDULE_API = "http://35.238.162.84/schedule/schedule/"
 export const AUTH_API = "http://35.239.173.63/account/api/token/";
