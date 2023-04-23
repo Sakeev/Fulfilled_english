0bc2368bf4618f850a8e0c888b8b784865a9aee4
Sultan <bekovs96@gmail.com>
2023-04-24 02:05:48 +0600

    .

--- src/components/classwork/tasks/Table.jsx
+++ src/components/classwork/tasks/Table.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Table = ({task}) => {
-  console.log(task[0]?.description)
+  console.log(task[0]?.description.split('_th'))
+  const [tableProps, setTableProps] = useState({
+    rows: task[0]?.description.split('\r\n')[0].split('x')[1],
+    cells: task[0]?.description.split('\r\n')[0].split('x')[0],
+  })
   return (
     <table>
-      {
-        
-      }
+      <thead>
+        <tr>
+          {
+            Array.from({ length: tableProps.cells }, (_, i) => (
+              <th style={{border: '1px solid black', width: '100%', minWidth: '100px'}} key={`header-${i}`}>Column {i + 1}</th>
+            ))
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {
+          Array.from({ length: tableProps.rows - 1 }, (_, i) => (
+            <tr key={i}>
+              {
+                Array.from({ length: tableProps.cells },(_, i) => (
+                  <td style={{border: '1px solid black', width: '100%', minWidth: '100px'}} key={`header-${i}`}>row</td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </tbody>
     </table>
   );
 };
