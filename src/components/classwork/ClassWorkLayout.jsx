520b085568c8274252dddc7b0d174bf4cc777912
Саламат Кадыров <salamat@MacBook-Air-Salamat.local>
2023-05-07 18:18:06 +0600

    notes classwork

--- src/components/classwork/ClassWorkLayout.jsx
+++ src/components/classwork/ClassWorkLayout.jsx
@@ -1,163 +1,232 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { BtnBold, BtnBulletList, BtnClearFormatting, BtnItalic, BtnLink, BtnNumberedList, BtnRedo, BtnStrikeThrough, BtnStyles, BtnUnderline, BtnUndo, DefaultEditor, Editor, EditorProvider, HtmlButton, Separator, Toolbar } from 'react-simple-wysiwyg';
-import { ReadyState } from 'react-use-websocket';
-import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
-import { isTeacher } from '../../helpers/funcs';
-import { Button } from '@mui/material';
-import ClassTasks from './ClassTasks';
-import { useClassWork } from '../../contexts/ClassWorkContextProvider';
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  BtnBold,
+  BtnBulletList,
+  BtnClearFormatting,
+  BtnItalic,
+  BtnLink,
+  BtnNumberedList,
+  BtnRedo,
+  BtnStrikeThrough,
+  BtnStyles,
+  BtnUnderline,
+  BtnUndo,
+  DefaultEditor,
+  Editor,
+  EditorProvider,
+  HtmlButton,
+  Separator,
+  Toolbar,
+} from "react-simple-wysiwyg";
+import { ReadyState } from "react-use-websocket";
+import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
+import { isTeacher } from "../../helpers/funcs";
+import { Button } from "@mui/material";
+import ClassTasks from "./ClassTasks";
+import { useClassWork } from "../../contexts/ClassWorkContextProvider";
 
 const request_id = new Date().getTime();
 
 function isDocumentEvent(message) {
   let evt = JSON.parse(message.data);
-  return evt.type === 'contentchange';
+  return evt.type === "contentchange";
 }
 // const room_pk = 1;
 const ClassWorkLayout = () => {
-  const { room_pk } = useClassWork();
-  const [socketUrl, setSocketUrl] = useState(`ws://35.239.173.63/ws/chat/?token=${JSON.parse(localStorage.getItem('token')).access}`);
-  const [lesson, setLesson] = useState({})
-  const [html, setHtml] = useState('');
-  const [inps, setInps] = useState('');
+  const { room_pk, postNote } = useClassWork();
+  const [socketUrl, setSocketUrl] = useState(
+    `ws://35.239.173.63/ws/chat/?token=${
+      JSON.parse(localStorage.getItem("token")).access
+    }`
+  );
+  const [lesson, setLesson] = useState({});
+  const [html, setHtml] = useState("");
+  const [inps, setInps] = useState("");
   const [playing, setPlaying] = useState(false);
+  const [lessonId, setLessonId] = useState(0);
 
-  const tasks = useCallback((data) => {
-    setLesson(data);
-  }, [lesson])
+  const tasks = useCallback(
+    (data) => {
+      setLesson(data);
+    },
+    [lesson]
+  );
 
-  const { sendJsonMessage, readyState, lastJsonMessage } = useWebSocket(socketUrl , {
-    onOpen: () => {
-      console.log('WebSocket connection established.');
-      const joinRoom = {
+  const { sendJsonMessage, readyState, lastJsonMessage } = useWebSocket(
+    socketUrl,
+    {
+      onOpen: () => {
+        console.log("WebSocket connection established.");
+        const joinRoom = {
           pk: room_pk,
           action: "join_room",
           request_id: request_id,
-      };
-      sendJsonMessage(joinRoom);
-      const retrieveMessage = {
+        };
+        sendJsonMessage(joinRoom);
+        const retrieveMessage = {
           pk: room_pk,
           action: "retrieve",
           request_id: request_id,
-      };
-      sendJsonMessage(retrieveMessage);
-      const subscribeMessage = {
+        };
+        sendJsonMessage(retrieveMessage);
+        const subscribeMessage = {
           pk: room_pk,
           action: "subscribe_to_messages_in_room",
           request_id: request_id,
-      };
-      sendJsonMessage(subscribeMessage);
-      const subscribeInstanceMessage = {
+        };
+        sendJsonMessage(subscribeMessage);
+        const subscribeInstanceMessage = {
           pk: room_pk,
           action: "subscribe_instance",
           request_id: request_id,
-      };
-      sendJsonMessage(subscribeInstanceMessage);
-    },
-    onMessage: (e) => {
-      const data = JSON.parse(e.data);
+        };
+        sendJsonMessage(subscribeInstanceMessage);
+      },
+      onMessage: (e) => {
+        const data = JSON.parse(e.data);
         switch (data.action) {
-          case 'retrieve':
+          case "retrieve":
             console.log(data.data);
-            tasks(data.data.lesson)
+            setLessonId(data.data.lesson.id);
+            tasks(data.data.lesson);
             break;
-          case 'create':
-            if(!isTeacher()){
-              setHtml(data.data.text)
+          case "create":
+            if (!isTeacher()) {
+              setHtml(data.data.text);
             }
             console.log(data.action, data.data);
             break;
           default:
             break;
         }
-    },
-    share: true,
-    filter: isDocumentEvent,
-    retryOnError: false,
-    onClose: (e) => console.log(e),
-    shouldReconnect: () => false
-  });
+      },
+      share: true,
+      filter: isDocumentEvent,
+      retryOnError: false,
+      onClose: (e) => console.log(e),
+      shouldReconnect: () => false,
+    }
+  );
 
   useEffect(() => {
     sendJsonMessage({
       playing: playing,
       action: "audio_play",
       request_id: request_id,
-    })
-  }, [playing])
-
+    });
+  }, [playing]);
 
   useEffect(() => {
-    const timeOut = setTimeout(() => sendJsonMessage({
-      message: html,
-      action: "create_message",
-      request_id: request_id,
-    }), 300);
+    const timeOut = setTimeout(
+      () =>
+        sendJsonMessage({
+          message: html,
+          action: "create_message",
+          request_id: request_id,
+        }),
+      300
+    );
     return () => clearTimeout(timeOut);
-  }, [html])
-  
+  }, [html]);
+
   useEffect(() => {
     sendJsonMessage({
       message: inps,
       action: "create_message",
       request_id: request_id,
-    })
-  }, [inps])
-  
+    });
+  }, [inps]);
+
   const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
   }[readyState];
 
   function handleHtmlChange(e) {
-    setHtml(e.target.value);;
+    setHtml(e.target.value);
   }
 
   function handleInputsChange(e) {
-    setInps(e.target.value)
+    setInps(e.target.value);
   }
 
+  function sendNote() {
+    let note = {
+      body: html,
+      lesson: lessonId,
+    };
+
+    postNote(note);
+  }
   return (
-    <div style={{ width: '80%', height: '95vh', margin: '40px 0 0 30px', display: 'flex' }}>
-      <div style={{ width: '40%', height: '100%', minWidth: '300px' }}>
-        <div style={{ height: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div
+      style={{
+        width: "80%",
+        height: "95vh",
+        margin: "40px 0 0 30px",
+        display: "flex",
+      }}
+    >
+      <div style={{ width: "40%", height: "100%", minWidth: "300px" }}>
+        <div
+          style={{
+            height: "50%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Button color="warning">Zoom link</Button>
         </div>
         <span>The WebSocket is currently {connectionStatus}</span>
 
         <EditorProvider>
-          <Editor containerProps={{ style: { height: '40vh', maxHeight: '500px', width: '100%' } }} value={html} onChange={handleHtmlChange} disabled={!isTeacher()}>
-            {
-              isTeacher() ?
+          <Editor
+            containerProps={{
+              style: { height: "40vh", maxHeight: "500px", width: "100%" },
+            }}
+            value={html}
+            onChange={handleHtmlChange}
+            disabled={!isTeacher()}
+          >
+            {isTeacher() ? (
               <Toolbar>
-              <BtnUndo />
-              <BtnRedo />
-              <Separator />
-              <BtnBold />
-              <BtnItalic />
-              <BtnUnderline />
-              <BtnStrikeThrough />
-              <Separator />
-              <BtnNumberedList />
-              <BtnBulletList />
-              <Separator />
-              <BtnLink />
-              <BtnClearFormatting />
-              <HtmlButton />
-              <Separator />
-              <BtnStyles />
-            </Toolbar>
-            :
-            <></>
-            }
+                <BtnUndo />
+                <BtnRedo />
+                <Separator />
+                <BtnBold />
+                <BtnItalic />
+                <BtnUnderline />
+                <BtnStrikeThrough />
+                <Separator />
+                <BtnNumberedList />
+                <BtnBulletList />
+                <Separator />
+                <BtnLink />
+                <BtnClearFormatting />
+                <HtmlButton />
+                <Separator />
+                <BtnStyles />
+                <Button onClick={sendNote} color="success">
+                  Send Note
+                </Button>
+              </Toolbar>
+            ) : (
+              <></>
+            )}
           </Editor>
         </EditorProvider>
       </div>
-      <div style={{ margin: '0 30px', width: '70%' }}>
-        <ClassTasks lesson={lesson} playing={playing} setPlaying={setPlaying} handleInputsChange={handleInputsChange} />
+      <div style={{ margin: "0 30px", width: "70%" }}>
+        <ClassTasks
+          lesson={lesson}
+          playing={playing}
+          setPlaying={setPlaying}
+          handleInputsChange={handleInputsChange}
+        />
       </div>
     </div>
   );
--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,8 +1,9 @@
-export const ADMIN = 'admin@admin.com';
+export const ADMIN = "admin@admin.com";
 
-export const API = 'http://35.239.173.63/';
+export const API = "http://35.239.173.63/";
 // export const AUTH_API = "http://35.239.173.63/account/api/token/";
-export const SCHEDULE_API = 'http://35.239.173.63/schedule/schedule/';
-export const AUTH_API = 'http://35.239.173.63/account/api/token/';
-export const CHAT_ROOM_API = 'http://35.239.173.63/chat/room/';
-export const ROOM_API = 'http://35.239.173.63/room/';
+export const SCHEDULE_API = "http://35.239.173.63/schedule/schedule/";
+export const AUTH_API = "http://35.239.173.63/account/api/token/";
+export const CHAT_ROOM_API = "http://35.239.173.63/chat/room/";
+export const CHAT_NOTES_API = "http://35.239.173.63/chat/notes/";
+export const ROOM_API = "http://35.239.173.63/room/";
