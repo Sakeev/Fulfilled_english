42f05bbddfac66900a2e4253846183a8489debb6
Jaanger123 <jbarakanov@inbox.ru>
2023-06-17 21:09:06 +0600

    merge

--- src/components/classwork/ClassTasks.jsx
+++ src/components/classwork/ClassTasks.jsx
@@ -1,127 +1,128 @@
-import React, { useEffect, useState } from "react";
-import Vocabulary from "./tasks/Vocabulary";
-import Listening from "./tasks/Listening";
-import Table from "./tasks/Table";
-import { Box } from "@mui/system";
-import FlInps from "./tasks/FlInps";
-import Describing from "./tasks/Describing";
+import React, { useEffect, useState } from 'react';
+import Vocabulary from './tasks/Vocabulary';
+import Listening from './tasks/Listening';
+import Table from './tasks/Table';
+import { Box } from '@mui/system';
+import FlInps from './tasks/FlInps';
+import Describing from './tasks/Describing';
 
 const ClassTasks = ({
-  lesson,
-  playing,
-  setPlaying,
-  handleInputsChange,
-  sendJsonMessage,
-  inps,
-  setInps,
-  setTyping,
-  request_id,
+    lesson,
+    playing,
+    setPlaying,
+    handleInputsChange,
+    sendJsonMessage,
+    inps,
+    setInps,
+    setTyping,
+    request_id,
 }) => {
-  const renderTask = (task) => {
-    switch (task.title.toLowerCase()) {
-      case "listening":
-        return (
-          <Listening
-            task={task.tasks}
-            playing={playing}
-            sendJsonMessage={sendJsonMessage}
-            request_id={request_id}
-            listeningId={task.id}
-            taskId={task.tasks[0].id}
-          />
-        );
-      case "table_exercise":
-        return (
-          <Table
-            task={task.tasks}
-            handleInputsChange={handleInputsChange}
-            lesson={lesson}
-            sendJsonMessage={sendJsonMessage}
-            inps={inps}
-            setInps={setInps}
-            setTyping={setTyping}
-          />
-        );
-      case "flinps":
-        return (
-          <FlInps
-            task={task.tasks}
-            inps={inps}
-            setInps={setInps}
-            setTyping={setTyping}
-          />
-        );
-      case "describing":
-        return <Describing task={task.tasks} />;
-      default:
-        return <></>;
-    }
-  };
+    const renderTask = (task) => {
+        // console.log(task);
+        switch (task.title.toLowerCase()) {
+            case 'listening':
+                return (
+                    <Listening
+                        task={task.tasks}
+                        playing={playing}
+                        sendJsonMessage={sendJsonMessage}
+                        request_id={request_id}
+                        listeningId={task.id}
+                        taskId={task.tasks[0].id}
+                    />
+                );
+            case 'table_exercise':
+                return (
+                    <Table
+                        task={task.tasks}
+                        handleInputsChange={handleInputsChange}
+                        lesson={lesson}
+                        sendJsonMessage={sendJsonMessage}
+                        inps={inps}
+                        setInps={setInps}
+                        setTyping={setTyping}
+                    />
+                );
+            case 'flinps':
+                return (
+                    <FlInps
+                        task={task.tasks}
+                        inps={inps}
+                        setInps={setInps}
+                        setTyping={setTyping}
+                    />
+                );
+            case 'describing':
+                return <Describing task={task.tasks} />;
+            default:
+                return <></>;
+        }
+    };
 
-  const [tasksQuan, setTasksQuan] = useState(1); // unit 2 also
-  const [tasks, setTasks] = useState([]);
+    const [tasksQuan, setTasksQuan] = useState(1); // unit 2 also
+    const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    if (lesson.case_tasks) {
-      setTasks(
-        lesson?.case_tasks?.unit1
-          ?.concat(lesson?.case_tasks?.unit2)
-          .filter(({ title }) => title.toLowerCase() !== "vocabulary")
-      );
-    }
-  }, [lesson]);
+    useEffect(() => {
+        if (lesson.case_tasks) {
+            setTasks(
+                lesson?.case_tasks?.unit1
+                    ?.concat(lesson?.case_tasks?.unit2)
+                    .filter(({ title }) => title.toLowerCase() !== 'vocabulary')
+            );
+        }
+    }, [lesson]);
 
-  useEffect(() => {
-    if (tasks.length) {
-      setTasksQuan(tasks.length);
-    }
-  }, [tasks]);
+    useEffect(() => {
+        if (tasks.length) {
+            setTasksQuan(tasks.length);
+        }
+    }, [tasks]);
 
-  const handlePaginationBtn = (direction) => {
-    if (
-      (tasksQuan <= activePage && direction !== -1) ||
-      (activePage <= 1 && direction !== 1)
-    ) {
-      return;
-    }
-    setActivePage(activePage + direction);
-  };
+    const handlePaginationBtn = (direction) => {
+        if (
+            (tasksQuan <= activePage && direction !== -1) ||
+            (activePage <= 1 && direction !== 1)
+        ) {
+            return;
+        }
+        setActivePage(activePage + direction);
+    };
 
-  const [activePage, setActivePage] = useState(1);
-  return (
-    <>
-      <div className="slider__container">
-        <div className="slider__pagination">
-          <button
-            onClick={() => handlePaginationBtn(-1)}
-            disabled={activePage === 1 ? true : false}
-          >
-            &#8249;&#8249;
-          </button>
-          <h5>{activePage}</h5>
-          <button
-            onClick={() => handlePaginationBtn(1)}
-            disabled={activePage === tasksQuan ? true : false}
-          >
-            &#8250;&#8250;
-          </button>
-        </div>
-        {tasks?.map((task, ind) => (
-          <div
-            style={{ margin: "20px 0", width: "100%" }}
-            className={
-              activePage === ind + 1
-                ? "slider__page slider__page_active"
-                : "slider__page"
-            }
-            key={ind}
-          >
-            {renderTask(task)}
-          </div>
-        ))}
-      </div>
-    </>
-  );
+    const [activePage, setActivePage] = useState(1);
+    return (
+        <>
+            <div className="slider__container">
+                <div className="slider__pagination">
+                    <button
+                        onClick={() => handlePaginationBtn(-1)}
+                        disabled={activePage === 1 ? true : false}
+                    >
+                        &#8249;&#8249;
+                    </button>
+                    <h5>{activePage}</h5>
+                    <button
+                        onClick={() => handlePaginationBtn(1)}
+                        disabled={activePage === tasksQuan ? true : false}
+                    >
+                        &#8250;&#8250;
+                    </button>
+                </div>
+                {tasks?.map((task, ind) => (
+                    <div
+                        style={{ margin: '20px 0', width: '100%' }}
+                        className={
+                            activePage === ind + 1
+                                ? 'slider__page slider__page_active'
+                                : 'slider__page'
+                        }
+                        key={ind}
+                    >
+                        {renderTask(task)}
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default ClassTasks;
--- src/components/classwork/tasks/Vocabulary.jsx
+++ src/components/classwork/tasks/Vocabulary.jsx
@@ -1,40 +1,45 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Typography,
+} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import React, { useEffect, useState } from 'react';
 
-const Vocabulary = ({vocabTasks = []}) => {
-  const [words, setWords] = useState([]);
+const Vocabulary = ({ vocabTasks = [] }) => {
+    const [words, setWords] = useState([]);
 
-  useEffect(() => {
-    if(vocabTasks.length){
-      let temp = vocabTasks.map(({tasks}) => tasks[0].description)
-      setWords([...new Set(temp.flat(Infinity))])
-    }
-  }, [vocabTasks])
+    useEffect(() => {
+        if (vocabTasks.length) {
+            let temp = vocabTasks.map(({ tasks }) => tasks[0]?.description);
+            setWords([...new Set(temp.flat(Infinity))]);
+        }
+    }, [vocabTasks]);
 
-  return (
-    <>
-      <Accordion sx={{boxShadow: 'none'}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{padding: '0'}}
-          >
-          <h2>Vocabulary</h2>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div className='vocabulary-box'>
-            {
-              words?.map((word, index) => (
-                <p className='vocabulary-word' key={index}>{word.toLowerCase().replace("_", " ")}</p>
-              ))
-            }
-          </div>
-        </AccordionDetails>
-      </Accordion>
-    </>
-  );
+    return (
+        <>
+            <Accordion sx={{ boxShadow: 'none' }}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    sx={{ padding: '0' }}
+                >
+                    <h2>Vocabulary</h2>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className="vocabulary-box">
+                        {words?.map((word, index) => (
+                            <p className="vocabulary-word" key={index}>
+                                {word?.toLowerCase().replace('_', ' ')}
+                            </p>
+                        ))}
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+        </>
+    );
 };
 
 export default Vocabulary;
