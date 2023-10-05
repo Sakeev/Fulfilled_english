81a19c76b2896019e70db2a2cca53ed1ef67cec2
Sultan Bekov <bekovs@Sultans-MacBook-Air.local>
2023-10-06 01:38:35 +0600

    .

--- src/components/teachers/ScheduleWindow.jsx
+++ src/components/teachers/ScheduleWindow.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
 function ScheduleWindow({lesson}) {
+  console.log(lesson)
   return (
     <div className="lesson_block" id={lesson.id}>
         <p id={lesson.id}>{lesson.user}</p>
-        <p id={lesson.id}>
-            <b id={lesson.id}>
-                {lesson.accepted ? "accepted" : "not accepted"}
-            </b>
-        </p>
         <p id={lesson.id} className='lesson_block__time'>
             <b>{lesson?.time?.split(":")?.splice(0, 2).join(":")}</b>
         </p>
