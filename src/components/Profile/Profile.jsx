1660d8a142f4bce720e555c9963b681ced2bb4a0
Jaanger123 <jbarakanov@inbox.ru>
2023-10-28 04:16:44 +0600

    Profile changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,73 +1,85 @@
-import { useEffect } from "react";
-import { useUsers } from "contexts/UsersContextProvider";
-import { capitalize, isTeacher } from "helpers/funcs";
-import { API } from "helpers/consts";
+import { useEffect } from 'react';
+import { useUsers } from 'contexts/UsersContextProvider';
+import { capitalize, isTeacher, lvlcheck } from 'helpers/funcs';
+import { API } from 'helpers/consts';
 
-import styles from "./Profile.module.scss";
+import styles from './Profile.module.scss';
 
 const Profile = () => {
-  const {
-    user,
-    studentProgress,
-    getStudentProgress,
-    getUser,
-    getUserAndProgress,
-  } = useUsers();
+    const { user, studentProgress, getUserAndProgress } = useUsers();
 
-  useEffect(() => {
-    // getStudentProgress();
-    // getUser();
-    getUserAndProgress();
-  }, []);
+    useEffect(() => {
+        getUserAndProgress();
+    }, []);
 
-  const getPercents = () => {
-    return Math.round(
-      (100 / studentProgress.lessons.length) * studentProgress.progres_classwork
-    );
-  };
+    const getPercents = () => {
+        return Math.round(
+            (100 / studentProgress.lessons.length) *
+                studentProgress.progres_classwork
+        );
+    };
 
-  console.log(studentProgress);
-  console.log(user);
+    console.log(studentProgress);
+    console.log(user);
 
-  if (!user) return <></>;
+    if (!user) return <></>;
 
-  return (
-    <div className={styles.profileContainer}>
-      <div className={styles.userInfo}>
-        <div className={styles.userAvatar}>
-          <img src={`${API}${user.avatar}`} alt="user" />
-        </div>
-        <div className={styles.userInfoCard}>
-          <div className={styles.userFullName}>
-            <h2>
-              {user.first_name} {user.last_name}
-            </h2>
-            <span>{isTeacher() ? "Teacher" : "Student"}</span>
-          </div>
-          <div className={styles.courseInfo}>
-            <p>
-              Email: <span>{user.email}</span>
-            </p>
-            <p>
-              Level: <span>{capitalize(studentProgress.level)}</span>
-            </p>
-            {!isTeacher() && (
-              <div className={styles.studentProgress}>
-                <span>Progress</span>
-                <div className={styles.progress}>
-                  <span className={styles.percent}>{getPercents()}%</span>
-                  <div
-                    className={styles.progressBar}
-                    style={{ width: getPercents() + "%" }}
-                  ></div>
+    const { logo, level } = lvlcheck(studentProgress.level);
+
+    return (
+        <div className={styles.profileContainer}>
+            <div className={styles.userInfo}>
+                <div className={styles.userAvatar}>
+                    <img src={`${API}${user.avatar}`} alt="user" />
                 </div>
-              </div>
-            )}
-          </div>
+                <div className={styles.userInfoCardWrapper}>
+                    <div className={styles.userInfoCard}>
+                        <div className={styles.userFullName}>
+                            <h2>
+                                {user.first_name} {user.last_name}
+                            </h2>
+                            <span>{isTeacher() ? 'Teacher' : 'Student'}</span>
+                        </div>
+                        <div className={styles.courseInfo}>
+                            <p>
+                                Email: <span>{user.email}</span>
+                            </p>
+                            {!isTeacher() && (
+                                <p>
+                                    Payment left:{' '}
+                                    <span>{studentProgress.payment}</span>
+                                </p>
+                            )}
+                            {!isTeacher() && (
+                                <div className={styles.studentProgress}>
+                                    <span>Progress</span>
+                                    <div className={styles.progress}>
+                                        <span className={styles.percent}>
+                                            {getPercents()}%
+                                        </span>
+                                        <div
+                                            className={styles.progressBar}
+                                            style={{
+                                                width: getPercents() + '%',
+                                            }}
+                                        ></div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className={styles.level}>
+                        <p>
+                            Level: <span>{level}</span>
+                        </p>
+                        <div className={styles.emblem}>
+                            <img src={logo} alt="level" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Profile;
